6e8ac81e972c6a029fb83654ecd64d6e2c27bf03
Arturo Barrantes Vasquez <arturo.barrantesv@pucp.pe>
2024-05-19 16:16:21 -0500

    summary mode

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
 from langchain.chains import LLMChain
-from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+from langchain.memory import ConversationSummaryMemory, FileChatMessageHistory
 from dotenv import load_dotenv
 
 load_dotenv()
 
-chat = ChatOpenAI()
+chat = ChatOpenAI(verbose=True)
 
-memory = ConversationBufferMemory(
-    chat_memory=FileChatMessageHistory("messages.json"), 
-    memory_key='messages',return_messages = True )
+memory = ConversationSummaryMemory(
+    #chat_memory=FileChatMessageHistory("messages.json"), 
+    memory_key='messages',
+    return_messages = True,
+    llm=chat
+    )
 """ ConversationBufferMemory this works with chat base models"""
 
 prompt = ChatPromptTemplate(
@@ -24,7 +27,8 @@ prompt = ChatPromptTemplate(
 chain = LLMChain(
     llm=chat,
     prompt = prompt, 
-    memory = memory
+    memory = memory,
+    verbose=True,
 )
 
 while True:
